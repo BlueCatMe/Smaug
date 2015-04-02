e411892447576f2e2c762249acae1b5da51fc2be
Jimmy.CM_Chen <jimmy.cm_chen@htc.com>
2015-04-02 11:58:14 +0800

    refactor: log: re-arrange codes and ignore google api logs.

--- main.py
+++ main.py
@@ -23,17 +23,7 @@ stream_handler.setFormatter(stream_formatter)
 stream_handler.addFilter(StreamHandlerFilter())
 logger.addHandler(stream_handler)
 
-def main(argv):
-
-	# translate encoding from file system to unicode.
-	argv = [a.decode(sys.getfilesystemencoding()) for a in argv]
-
-	client_secret_json_path = os.path.join(os.path.dirname(argv[0]), CLIENT_SECRET_JSON_FILENAME)
-	credentials_storage_path = os.path.join(os.path.dirname(argv[0]), CREDENTIALS_STORAGE_FILENAME)
-
-	if not os.path.isfile(client_secret_json_path):
-		logger.critical(u"Please prepare %s for Google Drive API usage." % CLIENT_SECRET_JSON_FILENAME)
-		return -1
+def process_argument(argv):
 
 	parser = argparse.ArgumentParser(description=u'Batch upload files to Google Drive')
 	parser.add_argument(u'targets', nargs=u'+',
@@ -42,8 +32,10 @@ def main(argv):
 			default=False, help=u"Do not recreate folder structure in Google Drive.")
 	parser.add_argument(u'--request-new-credentials', action='store_true',
 			default=False, help=u"Request new credentials to change account.")
-	parser.add_argument(u'--credentials-path', default=credentials_storage_path,
+	parser.add_argument(u'--credentials-path', default=os.path.join(os.path.dirname(argv[0]), CREDENTIALS_STORAGE_FILENAME),
 			help=u"assign credentials file path.")
+	parser.add_argument(u'--client-secret-json-path', default=os.path.join(os.path.dirname(argv[0]), CLIENT_SECRET_JSON_FILENAME),
+			help=u"assign client secret json file path.")
 	parser.add_argument(u'--move-to-backup-folder', default=None,
 			help=u"Move uploaded file to a backup folder.")
 	parser.add_argument(u'--move-skipped-file', action=u'store_true',
@@ -54,7 +46,23 @@ def main(argv):
 			help=u"How to handle existing file with the same title")
 	parser.add_argument(u'--log-file', default=None,
 			help=u"The remote folder path to upload the documents separated by '/'.")
-	options = parser.parse_args(argv[1:])
+
+	return parser.parse_args(argv[1:])
+
+def main(argv):
+
+	# translate encoding from file system to unicode.
+	argv = [a.decode(sys.getfilesystemencoding()) for a in argv]
+
+	options = process_argument(argv)
+	logger.debug(options)
+
+	credentials_storage_path = options.credentials_path
+	client_secret_json_path = options.client_secret_json_path
+
+	if not os.path.isfile(client_secret_json_path):
+		logger.critical(u"Please prepare %s for Google Drive API usage." % CLIENT_SECRET_JSON_FILENAME)
+		return -os.errno.ENOENT
 
 	if options.log_file != None:
 		file_handler = logging.FileHandler(options.log_file, mode = u'w', encoding = u'utf-8')
@@ -63,31 +71,26 @@ def main(argv):
 		file_handler.setFormatter(file_formatter)
 		logger.addHandler(file_handler)
 
-	logger.debug(options)
-
-	credentials_storage_path = options.credentials_path
-
-	service = GoogleDriveService()
-
-	service.options[u'request_new_credentials'] = options.request_new_credentials
-	service.options[u'conflict_action'] = options.conflict_action
-	service.options[u'move_to_backup_folder'] = options.move_to_backup_folder
-	service.options[u'move_skipped_file'] = options.move_skipped_file
-
-	logger.debug(service.options)
-
 	if options.move_to_backup_folder != None:
 		if not os.path.exists(options.move_to_backup_folder):
 			try:
 				os.makedirs(options.move_to_backup_folder)
 			except OSError, err:
 				logger.critical(u"Cannot create backup folder `%s'" % options.move_to_backup_folder)
-				return -1
+				return -os.errno.EBUSY
 
 		if not os.path.isdir(options.move_to_backup_folder):
 			logger.critical(u"`%s' is not a folder." % options.move_to_backup_folder)
-			return -1
+			return -os.errno.ENOTDIR
 
+	service = GoogleDriveService()
+	service.options[u'request_new_credentials'] = options.request_new_credentials
+	service.options[u'conflict_action'] = options.conflict_action
+	service.options[u'move_to_backup_folder'] = options.move_to_backup_folder
+	service.options[u'move_skipped_file'] = options.move_skipped_file
+	logger.debug(service.options)
+
+	ret_code = 0
 	if service.authorize(client_secret_json_path, credentials_storage_path):
 		for target in options.targets:
 			result = service.upload(target,
@@ -97,6 +100,9 @@ def main(argv):
 				logger.info(u"Uploading `{0}' successed.".format(target));
 			else:
 				logger.warn(u"Uploading `{0}' failed.".format(target));
+				ret_code = -os.errno.EIO
+
+	return ret_code
 
 if __name__ == u"__main__":
 	main(sys.argv)
