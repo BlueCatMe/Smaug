c2df3a9cc7486bc95728db6454a779a83f1906e2
Jimmy Chen <bluecat.me@gmail.com>
2015-08-22 15:05:40 +0800

    show: support --find-path to show the path of the item

--- actions/show.py
+++ actions/show.py
@@ -19,7 +19,8 @@ logger = logging.getLogger(__name__)
 @register_action(u'show', 'Show details of an item.')
 class List(ActionBase):
 	def update_argparser(self, parser, argv):
-		pass
+		parser.add_argument(u'--find-path', action=u'store_true',
+				default=False, help=u"Find the path of the item.")
 
 	def execute(self, options):
 
@@ -28,5 +29,22 @@ class List(ActionBase):
 
 		return self.service.get(options.target)
 
+	def find_path(self, id):
+		item = self.service.get(id)
+		if item == None:
+			return
+		if len(item[u'parents']) == 0:
+			return
+
+		for p in item[u'parents']:
+			self.find_path(p[u'id'])
+			sys.stdout.write(u'/{0}'.format(item[u'title']))
+
 	def show(self, results, options):
 		pprint.pprint(results)
+
+		if options.find_path:
+			print u'Path:'
+			for p in results[u'parents']:
+				self.find_path(p[u'id'])
+				sys.stdout.write(u'/{0}\r\n'.format(results[u'title']))
